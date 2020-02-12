06ea981c1986ad2e5d502c48f5313d200c266b27
taiki-nishi <taiki-nishi@cybozu.co.jp>
2020-02-12 18:56:46 +0900

    consoleを出すまで

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,1 @@
-import api from "./lib/api";
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message) {
-    case "api":
-      api().then(json => {
-        sendResponse(json);
-        chrome.storage.local.set({ saved_time: Date.now() });
-      });
-      return true;
-    default:
-      return false;
-  }
-});
+console.log("back ground");
